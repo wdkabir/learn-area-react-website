399edbefebccdb46e8f4b5bc7b91184bef13f9ca
wdkabir <wdkabirhossain@gmail.com>
2021-11-21 18:38:15 +0600

    git origin changed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className="container-fluid bg-light p-2">
-            <p className="text text-dark text-center lh-lg">&copy; Learn Area 2021 || All Rights Reserved By Kabir</p>
+            <p className="text text-dark text-center lh-lg">&copy; Learn Area 2021 || All Rights Reserved By Kabir Hossain</p>
         </div>
     );
 };
